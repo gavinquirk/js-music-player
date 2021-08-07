30afbbb98a87245b8001ceeefb712b0807d30707
Gavin Quirk <gavinquirk@gmail.com>
2021-08-06 17:07:00 -0700

    cover img bug fix, prevSong functionality

--- script.js
+++ script.js
@@ -21,19 +21,35 @@ loadSong(songs[songIndex]);
 function loadSong(song) {
   title.innerText = song;
   audio.src = `music/${song}.mp3`;
-  cover.src = `images/${song}.mp3`;
+  cover.src = `images/${song}.jpg`;
 }
 
 function playSong() {
   musicContainer.classList.add('play');
   playBtn.querySelector('i.fas').classList.remove('fa-play');
   playBtn.querySelector('i.fas').classList.add('fa-pause');
+
+  audio.play();
 }
 
 function pauseSong() {
   musicContainer.classList.remove('play');
   playBtn.querySelector('i.fas').classList.add('fa-play');
   playBtn.querySelector('i.fas').classList.remove('fa-pause');
+
+  audio.pause();
+}
+
+function prevSong() {
+  songIndex--;
+
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+
+  loadSong(songs[songIndex]);
+
+  playSong();
 }
 
 // Event listeners
@@ -46,3 +62,6 @@ playBtn.addEventListener('click', () => {
     playSong();
   }
 });
+
+// Change song events
+prevBtn.addEventListener('click', prevSong);
